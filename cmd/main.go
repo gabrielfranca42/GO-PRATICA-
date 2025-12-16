210e3425ee337416b13c565cea40f295c9e6d691
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-12-16 10:47:15 -0300

    billy bat

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
-import(
+import (
+	"go-api/controller"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,12 +10,15 @@ func main(){
 
 	server := gin.Default()
 
+	ProductController := controller.NewProductController()
+
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 	 
+	server.GET("/product", ProductController.GetProducts)
 	server.Run(":3000")
 
 }
