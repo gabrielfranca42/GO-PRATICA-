45859c89d7363ca8a67aa3b73ab8064f8281d558
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-12-17 10:49:38 -0300

    billy bat

---
+++ usecase/product_usecase.go
@@ -0,0 +1,14 @@
+package usecase
+
+import "go-api/model"
+
+type productUsecase struct {
+}
+
+func NewProductUseCase() productUsecase{
+	return productUsecase{}
+}
+
+	func(pu *productUsecase) GetProducts() ([]model.Product, error) {
+		return []model.Product{}, nil 
+	}
