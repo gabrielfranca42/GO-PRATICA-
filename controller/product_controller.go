c145dcb7cab8dbf5590b1e703aca14ad348fbc87
Gabriel Eduardo de Moura Franca <gabrielfranca172@gmail.com>
2026-01-13 11:10:36 -0300

    xd

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -10,23 +10,21 @@ import (
 )
 
 type productController struct {
-	productUsecase usecase.ProductUsecase
+	productUseCase usecase.ProductUsecase
 }
 
 func NewProductController(usecase usecase.ProductUsecase) productController {
 	return productController{
-		productUsecase: usecase,
+		productUseCase: usecase,
 	}
 }
 
 func (p *productController) GetProducts(ctx *gin.Context) {
 
-	products := []model.Product{
-		{
-			ID:    1,
-			Name:  "Batata frita",
-			Price: 20,
-		},
+	products, err := p.productUseCase.GetProducts()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
 	}
 
 	ctx.JSON(http.StatusOK, products)
@@ -40,41 +38,38 @@ func (p *productController) CreateProduct(ctx *gin.Context) {
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, err)
 		return
-
 	}
 
-	insertedProduct, err := p.productUsecase.CreateProduct(product)
+	insertedProduct, err := p.productUseCase.CreateProduct(product)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, err)
+		ctx.JSON(http.StatusInternalServerError, err)
 		return
 	}
 
 	ctx.JSON(http.StatusCreated, insertedProduct)
 }
 
-func (p *productController) GetProductByid(ctx *gin.Context) {
+func (p *productController) GetProductById(ctx *gin.Context) {
+
 	id := ctx.Param("productId")
 	if id == "" {
 		response := model.Response{
-			Message: "id do produto nao pode ser nulo",
+			Message: "Id do produto nao pode ser nulo",
 		}
-
 		ctx.JSON(http.StatusBadRequest, response)
 		return
-
 	}
 
-	productoId, err := strconv.Atoi(id)
+	productId, err := strconv.Atoi(id)
 	if err != nil {
 		response := model.Response{
-			Message: "id do produto precisa ser um numero",
+			Message: "Id do produto precisa ser um numero",
 		}
-
 		ctx.JSON(http.StatusBadRequest, response)
 		return
 	}
 
-	product, err := p.productUsecase.GetProductByid(productoId)
+	product, err := p.productUseCase.GetProductById(productId)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, err)
 		return
@@ -82,12 +77,11 @@ func (p *productController) GetProductByid(ctx *gin.Context) {
 
 	if product == nil {
 		response := model.Response{
-			Message: "produto nao foi encontrado na base de dados",
+			Message: "Produto nao foi encontrado na base de dados",
 		}
 		ctx.JSON(http.StatusNotFound, response)
 		return
 	}
 
 	ctx.JSON(http.StatusOK, product)
-
 }
